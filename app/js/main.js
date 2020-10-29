16d8caca00f12cdc2f1bb64f1df20870e8dba2d3
m-diana-dev <m.diana.dev@gmail.com>
2020-10-29 20:49:33 +0300

    added page new-items

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,32 @@
 $(function(){
-    var mixer = mixitup('.products__inner-box');
+    $('.icon-th-list').on('click', () => {
+        $('.products-page__items .product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+      });
+    
+    $('.icon-th-large').on('click', () => {
+        $('.products-page__items .product__item').removeClass('list');
+        $('.icon-th-list').removeClass('active');
+        $('.icon-th-large').addClass('active');
+      });
 
+    $('.js-range-slider').ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600
+      });
+
+      
     $(".rate-star").rateYo({
         rating: 5,
         starWidth: "11px",
         readOnly: true
       });
+
+    var mixer = mixitup('.products__inner-box');
 
     $('.product-slider__inner').slick({
         dots: true,
@@ -13,4 +34,5 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 4
     });
+   
 });
